6031b2048997cf9d0c644ec863052e8c4a900b2c
Štěpán Kabelka <stepankabelka@seznam.cz>
2025-10-02 07:56:44 +0200

    done

---
+++ accounts/src/accounts/Warningcheck.java
@@ -0,0 +1,26 @@
+package accounts;
+
+import java.io.Console;
+import java.io.IOException;
+import java.util.Objects;
+
+public class Warningcheck {
+    public static void Check(double amount)
+    {
+        if (amount <= 0)
+        {
+            throw new IllegalArgumentException("Zadejte platnou hodnotu.");
+        } else if (amount >= 10000) {
+
+            System.out.println("Jste si jist/a že to je správná peněžní hodnota?");
+            Console cnsl
+                    = System.console();
+            String str = cnsl.readLine(
+                    "Enter string : ");
+            if (Objects.equals(str, "ne")) {
+                throw new IllegalArgumentException("false value was selected");
+            }
+
+        }
+    }
+}
